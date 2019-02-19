e534865f316d4acddc7b18996063783542704780
chenxiao07 <chenxiao07@baidu.com>
2019-02-19 13:58:04 +0800

    first publish

--- src/factory.ts
+++ src/factory.ts
@@ -1,12 +1,9 @@
 import * as ts from 'typescript';
-import {
-    Node
-} from 'typescript';
 
 /**
  * Gets a custom text range to use when emitting comments.
  */
-export function getStartsOnNewLine(node: Node) {
+export function getStartsOnNewLine(node: ts.Node) {
     const emitNode = node.emitNode;
     return emitNode && emitNode.startsOnNewLine;
 }
