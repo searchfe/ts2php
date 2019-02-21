a0931e8a4164dc0ac96bcf2a56269e1ee01d89f1
chenxiao07 <chenxiao07@baidu.com>
2019-02-21 14:24:49 +0800

    add types for php global functions

---
+++ types/index.d.ts
@@ -0,0 +1,8 @@
+
+declare function date(x: string, t?: number): string;
+
+declare function strtotime(x: string): number;
+
+declare function isset(x: any): boolean;
+
+declare function empty(x: any): boolean;
