48a0b77e21a41930bcccc75cd2cdb205d2e0a7ec
chenxiao07 <chenxiao07@baidu.com>
2019-03-13 17:47:25 +0800

    feat: support parse_url component parameter

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,7 +1,9 @@
 
 declare function date(x: string, t?: number): string;
 
-declare function strtotime(x: string): number;
+declare function strftime(a: string, b: number | string): string;
+
+declare function strtotime(a: string): number;
 
 declare function isset(x: any): boolean;
 
@@ -25,7 +27,13 @@ type UrlInfo = {
 };
 
 declare function parse_url(x: string): false | UrlInfo;
+declare function parse_url(x: string, component: number): false | string;
 
-declare function strftime(a: string, b: number | string): string;
-
-declare function strtotime(a: string): number;
+declare const PHP_URL_SCHEME = 1;
+declare const PHP_URL_HOST = 2;
+declare const PHP_URL_PORT = 3;
+declare const PHP_URL_USER = 4;
+declare const PHP_URL_PASS = 5;
+declare const PHP_URL_PATH = 6;
+declare const PHP_URL_QUERY = 7;
+declare const PHP_URL_FRAGMENT = 8;
