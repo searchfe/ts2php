12237853e682014818186fc3d406ac8f7d09ae72
meixg <meixg@foxmail.com>
2019-06-03 16:02:30 +0800

    feat: php runtime unit test

--- test/runtime/spec/Ts2Php_DateTest.php
+++ test/runtime/spec/Ts2Php_DateTest.php
@@ -49,4 +49,159 @@ final class Ts2Php_DateTest extends TestCase {
             13
         );
     }
+
+    public function testGetMonth() {
+        $date = new Ts2Php_Date($this->time);
+
+        $this->assertEquals(
+            $date->getMonth(),
+            4
+        );
+    }
+
+    public function testGetSeconds() {
+        $date = new Ts2Php_Date($this->time);
+
+        $this->assertEquals(
+            $date->getSeconds(),
+            52
+        );
+    }
+
+    public function testGetTime() {
+        $date = new Ts2Php_Date($this->time);
+
+        $this->assertEquals(
+            $date->getTime(),
+            1559290432000
+        );
+    }
+
+    public function testNow() {
+        $this->assertEquals(
+            Ts2Php_Date::now(),
+            time() * 1000
+        );
+    }
+
+    public function testParse() {
+        $this->assertEquals(
+            Ts2Php_Date::parse('2019-04-11 12:54:03'),
+            1554958443000
+        );
+    }
+
+    public function testSetDate() {
+        $date = new Ts2Php_Date($this->time);
+        $date->setDate(11);
+
+        $this->assertEquals(
+            $date->getDay(),
+            6
+        );
+
+        $date->setDate(3);
+
+        $this->assertEquals(
+            $date->getDay(),
+            5
+        );
+    }
+
+    public function testSetFullYear() {
+        $date = new Ts2Php_Date($this->time);
+        $date->setFullYear(2005);
+
+        $this->assertEquals(
+            $date->getFullYear(),
+            2005
+        );
+    }
+
+    public function testSetHours() {
+        $date = new Ts2Php_Date($this->time);
+        $date->setHours(13);
+
+        $this->assertEquals(
+            $date->getHours(),
+            13
+        );
+
+        $date->setHours(9);
+
+        $this->assertEquals(
+            $date->getHours(),
+            9
+        );
+    }
+
+    public function testSetMinutes() {
+        $date = new Ts2Php_Date($this->time);
+        $date->setMinutes(26);
+
+        $this->assertEquals(
+            $date->getMinutes(),
+            26
+        );
+
+        $date->setMinutes(7);
+
+        $this->assertEquals(
+            $date->getMinutes(),
+            7
+        );
+    }
+
+    public function testSetMonth() {
+        $date = new Ts2Php_Date('2019-01-02');
+        $date->setMonth(5);
+
+        $this->assertEquals(
+            $date->getMonth(),
+            5
+        );
+
+        $date->setMonth(11);
+
+        $this->assertEquals(
+            $date->getMonth(),
+            11
+        );
+    }
+
+    public function testSetSeconds() {
+        $date = new Ts2Php_Date($this->time);
+        $date->setSeconds(23);
+
+        $this->assertEquals(
+            $date->getSeconds(),
+            23
+        );
+
+        $date->setSeconds(5);
+
+        $this->assertEquals(
+            $date->getSeconds(),
+            5
+        );
+    }
+
+    public function testSetTime() {
+        $date = new Ts2Php_Date($this->time);
+        $date->setTime(1546434903000);
+
+        $this->assertEquals(
+            $date->getSeconds(),
+            3
+        );
+    }
+
+    public function testConstruct() {
+        $date = new Ts2Php_Date();
+
+        $this->assertNotEquals(
+            $date->getTime(),
+            0
+        );
+    }
 }
